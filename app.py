8fc4f09f4ddec1f927c429e6dd19319c95bef8d8
tavo135 <tavo1235@proton.me>
2026-09-21 11:28:11 -0500

    Fix typo in service account method name

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from datetime import datetime
 st.title("Conection Test")
 try:
     creds = dict(st.secrets["gcp_service_account"])
-    gc = gspread.service_Account_from_dict(creds)
+    gc = gspread.service_account_from_dict(creds)
     st = gc.open("WIN FTTH Leads")
     st.success(f"Connected to: {sh.title}")
     st.write("Worksheets found:", [ws.title for ws in sh.worksheets()])
